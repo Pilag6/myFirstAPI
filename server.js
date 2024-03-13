a9457a7429de427a183c8d7d61158c9b792b17dc
Pilag6 <pilag6@gmail.com>
2024-03-13 11:36:58 +0100

    Add initial project files

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+
+
+const app = express();
+const PORT = 1000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.get("/randomNumber", (req, res) => {
+    res.json(Math.floor(Math.random() * 100).toString());
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT http://localhost:${PORT}`);
+});
