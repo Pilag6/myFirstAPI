d207c02c8abc702673355a4b7cebeedc4cc0b39d
Pilag6 <pilag6@gmail.com>
2024-03-13 14:20:18 +0100

    Add Port env

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 import express from "express";
 import cors from "cors";
 
-
 const app = express();
-const PORT = 1000;
+const PORT = process.env.PORT
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 
 app.get("/", (req, res) => {
     res.send("Hello World");
