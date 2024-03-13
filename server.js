dbe9cfd5afd408f0fa02fa9257cd1c4699693e35
Pilag6 <pilag6@gmail.com>
2024-03-13 15:48:59 +0100

    Refactor server.js to include weather API endpoint

--- server.js
+++ server.js
@@ -1,21 +1,36 @@
 import express from "express";
 import cors from "cors";
 
+// console.log(process.env);
 const app = express();
-const PORT = process.env.PORT
+const API_KEY = process.env.API_KEY;
+const PORT = process.env.PORT;
+const environment = process.env.NODE_ENV;
 
+// const environment = process.argv[2]; // "prod(production)"
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.send("Hello World");
+    res.json("hello world");
 });
 
 app.get("/randomNumber", (req, res) => {
     res.json(Math.floor(Math.random() * 100).toString());
 });
 
+app.get("/weather/:location", async (req, res) => {
+    console.log(req.params.location);
+	const response = await fetch(`https://api.tomorrow.io/v4/weather/forecast?location=${req.params.location}&apikey=${API_KEY}`);
+	const data = await response.json();
+	res.send("Current temperature: " + data.timelines.minutely[0].values.temperature);
+	// res.json(data);
+})
+
 app.listen(PORT, () => {
-    console.log(`Server is running on PORT http://localhost:${PORT}`);
+    if (environment === "development") {
+        console.log(`server is running on http://localhost:${PORT}`);
+    } else {
+        console.log("server running on production");
+    }
 });
